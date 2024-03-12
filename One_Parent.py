cbfab41953040c4667df7776f31d3e61938bd303
Zelltress <showofthesoul@gmail.com>
2024-03-12 19:45:33 +0700

    update

--- One_Parent.py
+++ One_Parent.py
@@ -1,4 +1,4 @@
-MAX_MONTH = 12
+'''MAX_MONTH = 12
 name_month = ['', 'январе', 'феврале', 'марте', 'апреле', 'мае', 'июне',
         'июле', 'августе', 'сентябре', 'октябре', 'ноябре', 'декабре']
 
@@ -6,32 +6,31 @@ name_month = ['', 'январе', 'феврале', 'марте', 'апреле'
 def income_count():
     sum_of_income = 0
     for month in range(1, MAX_MONTH + 1):
-        value_in_month = float(input(f' {'Доход в '} {name_month[month]} [USD]: ')) #изменить на значение рулокал 'ru.TEXT_INCOME'
+        value_in_month = float(input(f' {name_month[month]} [USD]: ')) #изменить на значение рулокал ru.TEXT_INCOME
         sum_of_income += value_in_month
     return sum_of_income
 
 def tax_free_count():
     amount = 0
     for month in range(1, MAX_MONTH + 1):
-        free_amnt_mnth = float(input(f' {'Сумма не облагаемая налогом в '} {name_month[month]} [USD]: ')) #ru.FREE_TAX и INCOME
+        free_amnt_mnth = float(input(f' {name_month[month]} [USD]: ')) #ru.FREE_TAX и INCOME
         amount += free_amnt_mnth
     return amount
-
+'''
 taxes_percent = [0, .1, .15, .25, .28, .33, .35, .396]
 
-cast = input()
+cast = int(input())
 def one_parent(taxable_money):
     brackets_for_parent = [0, 12950, 49400, 127550, 206600, 405100, 432200]
     parent_tax = 0
     remains = taxable_money
-    for i in range(len(brackets_for_parent)):
-        if taxable_money > brackets_for_parent[i]:
-            parent_tax += taxes_percent[i]*(brackets_for_parent[i+1]-brackets_for_parent[i])
+    for i in range(len(brackets_for_parent)-1):
+        if taxable_money > brackets_for_parent[i+1]:
+            parent_tax += taxes_percent[i+1]*(brackets_for_parent[i+1]-brackets_for_parent[i])
             remains -= brackets_for_parent[i+1]-brackets_for_parent[i]
         else:
-            parent_tax += remains*taxes_percent[i]
-        break
-    print(parent_tax) #gtf
+            parent_tax += remains*taxes_percent[i+1]
+            break
     return parent_tax
 
 if cast == 3:
