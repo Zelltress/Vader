039bc5781de3784b79c5417401f47656353505b5
Tania <t.ilinykh1@g.nsu.ru>
2024-03-12 16:16:10 +0700

    rename income to sum_of_income and add variables

--- case_3.py
+++ case_3.py
@@ -3,8 +3,15 @@ MAX_MONTH = 12
 
 # Функция income_count() возвращает сумму доходов за год
 def income_count():
-    income = 0
+    sum_of_income = 0
     for month in range(1, MAX_MONTH + 1):
         value_in_month = float(input(f' {ru.TEXT_INCOME} {ru.INCOME[month]} [USD]: '))
-        income += value_in_month
-    return income
+        sum_of_income += value_in_month
+    return sum_of_income
+
+# tax_free_count - временное обозначение функции, которая будет вычислять сумму дохода, не облагаемую налогом
+
+
+income = income_count()
+tax_free = tax_free_count()
+income_for_taxes = income - tax_free
