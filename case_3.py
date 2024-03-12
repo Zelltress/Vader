577bf7c7f8c72f07b3cc60c8b02131ced82e6814
Tania <t.ilinykh1@g.nsu.ru>
2024-03-12 17:28:44 +0700

    add function, determining tax for a married couple

--- case_3.py
+++ case_3.py
@@ -18,3 +18,16 @@ income_for_taxes = income - tax_free
 
 taxes_percent = [0, .1, .15, .25, .28, .33, .35, .396]
 brackets_for_couple = [0, 18150, 73800, 148850, 226850, 405100, 457600]
+
+
+def tax_for_married_couple(taxable_money):
+    couple_tax = 0
+    left_money = taxable_money
+    for i in range(1, len(brackets_for_couple)):
+        if taxable_money > brackets_for_couple[i]:
+            couple_tax += (brackets_for_couple[i]-brackets_for_couple[i-1])*taxes_percent[i]
+            left_money -= brackets_for_couple[i]-brackets_for_couple[i-1]
+        else:
+            couple_tax += left_money*taxes_percent[i]
+            break
+    return couple_tax
