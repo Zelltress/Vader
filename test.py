81847a847a15d685f01c33dbd4ffd49df64f3edf
NastYouSha <a.ivanova18@g.nsu.ru>
2024-03-12 23:29:26 +0700

    make part of choice

--- test.py
+++ test.py
@@ -1,2 +1,6 @@
-import ru.local as ru
+import ru_local as ru
 nomber = int(input(ru.CHOICE))
+category = {1: 'Subject', 2: 'Married_couple', 3: 'Parent'}
+print(category.get(nomber))
+a = category.get(nomber)
+
