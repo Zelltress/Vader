7f375d6b1e94ba0b3f8bf34ea633903107f63650
Zelltress <showofthesoul@gmail.com>
2024-03-12 20:08:24 +0700

    add 1.1

--- case_3.py
+++ case_3.py
@@ -1,4 +1,10 @@
+cast = int(input())
+
 MAX_MONTH = 12
+
+taxes_percent = [0, .1, .15, .25, .28, .33, .35, .396]
+brackets_for_couple = [0, 18150, 73800, 148850, 226850, 405100, 457600]
+brackets_for_subject = [0, 9075, 36900, 89350, 186350, 405100, 406750]
 
 
 # Функция income_count() возвращает сумму доходов за год
@@ -11,7 +17,6 @@ def income_count():
 
 
 # tax_free_count - временное обозначение функции, которая будет вычислять сумму дохода, не облагаемую налогом
-
 def tax_free_count():
     amount = 0
     for month in range(1, MAX_MONTH + 1):
@@ -20,38 +25,40 @@ def tax_free_count():
     return amount
 
 
-income = income_count()
-tax_free = tax_free_count()
-income_for_taxes = income - tax_free
-
-taxes_percent = [0, .1, .15, .25, .28, .33, .35, .396]
-brackets_for_couple = [0, 18150, 73800, 148850, 226850, 405100, 457600]
-brackets_for_subject = [0, 9075, 36900, 89350, 186350, 405100, 406750]
-
-
 # Функция tax_for_married_couple(taxable_money) определяет величину годового налога на супружескую пару
 def tax_for_married_couple(taxable_money):
     couple_tax = 0
     left_money = taxable_money
     for i in range(1, len(brackets_for_couple)):
         if taxable_money > brackets_for_couple[i]:
-            couple_tax += (brackets_for_couple[i]-brackets_for_couple[i-1])*taxes_percent[i]
-            left_money -= brackets_for_couple[i]-brackets_for_couple[i-1]
+            couple_tax += (brackets_for_couple[i] - brackets_for_couple[i - 1]) * taxes_percent[i]
+            left_money -= brackets_for_couple[i] - brackets_for_couple[i - 1]
         else:
-            couple_tax += left_money*taxes_percent[i]
+            couple_tax += left_money * taxes_percent[i]
             break
     return couple_tax
 
 
 # Функция определяет величину годового налога на субъект
-def tax_for_subject(tax_money):
+def tax_for_subject(taxable_money):
     subject_tax = 0
-    leftover = tax_money
+    leftover = taxable_money
     for j in range(1, len(brackets_for_subject)):
-        if tax_money > brackets_for_subject[j]:
-            subject_tax += (brackets_for_subject[j]-brackets_for_subject[j-1])*taxes_percent[j]
-            leftover -= brackets_for_subject[j]-brackets_for_subject[j-1]
+        if taxable_money > brackets_for_subject[j]:
+            subject_tax += (brackets_for_subject[j] - brackets_for_subject[j - 1]) * taxes_percent[j]
+            leftover -= brackets_for_subject[j] - brackets_for_subject[j - 1]
         else:
-            subject_tax += leftover*taxes_percent[j]
+            subject_tax += leftover * taxes_percent[j]
             break
     return subject_tax
+
+
+def main():
+    income = income_count() - tax_free_count()
+    if cast == 1:
+        print(tax_for_married_couple(income))
+    elif cast == 2:
+        print(tax_for_subject(income))
+
+
+main()
